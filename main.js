fe35badeb57c9e139ceb6d252a02e63a60ccab51
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-03 22:57:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -389,10 +389,10 @@ ppgroup = 'https://i.ibb.co/RBx5SQC/avatar-group-large-v2.png?q=60'
             mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
             if (mek.key && mek.key.remoteJid === 'status@broadcast') return
             if (!XeonBotInc.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
-            if (mek.key.id.startsWith('Xeon') && mek.key.id.length === 16) return
+            if (mek.key.id.startsWith('toge') && mek.key.id.length === 16) return
             if (mek.key.id.startsWith('BAE5')) return
             m = smsg(XeonBotInc, mek, store)
-            require("./XeonCheems11")(XeonBotInc, m, chatUpdate, store)
+            require("./TOGE-MD")(XeonBotInc, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
